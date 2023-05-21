8b2fab414fd2b6888cc9923c21db8f2b9b549f3c
Adnan Adil <adnanadil529@gmail.com>
2023-05-21 21:16:50 +0300

    delete booking functionality added

--- src/routes/elements/Table.jsx
+++ src/routes/elements/Table.jsx
@@ -8,6 +8,11 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import NoBookings from "./NoBookings";
 import "./Table.css";
+import * as AiIcons from "react-icons/ai";
+import { db } from "../../firebase.utils";
+import { doc, deleteDoc } from "firebase/firestore";
+
+
 
 function Table({ data }) {
   const bookingsFound = useSelector(
@@ -21,6 +26,25 @@ function Table({ data }) {
       setSettingTrue(true);
     }, 1000);
   }, []);
+  
+  const selectedParkingLotID = useSelector(
+    (state) => state.firebaseSlice.selectedParkingLot
+  );
+
+  // This function is called when we press the delete icon 
+  const itemClicked = (parkingID) => {
+    // show the pop up window ... 
+    if (window.confirm('Are you sure you wish to delete the booking?')) {
+      console.log(`Carry out delete for item ${parkingID}`)
+      deleteBooking(parkingID)
+    }
+  };
+
+  // Function delete the violation
+  const deleteBooking = async(docID) => {
+    console.log(`Deleting this bookings ${docID} @ ${selectedParkingLotID}`)
+    await deleteDoc(doc(db, `reservations-${selectedParkingLotID}`, docID));
+  }
 
   // We will show a table based on the data that we get from the Home.js as props, these
   // props are filtered by the search field
@@ -35,6 +59,7 @@ function Table({ data }) {
               <th>Date</th>
               <th>Name</th>
               <th>Email</th>
+              <th>Delete</th>
             </tr>
             {data.map((item) => (
               <tr key={item.parkingID}>
@@ -43,6 +68,9 @@ function Table({ data }) {
                 <td>{item.date}</td>
                 <td>{item.userName}</td>
                 <td>{item.userEmail}</td>
+                <td>               
+                   <AiIcons.AiFillDelete className="delete-icon" onClick={() => itemClicked(item.parkingID)}/>
+                </td>
               </tr>
             ))}
           </tbody>
